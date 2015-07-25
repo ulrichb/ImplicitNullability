c23cc50bc9915e1279e3382b4e9ffe972c7804f1
Ulrich Buchgraber <ulrich.b@gmx.at>
2015-07-25 22:30:44 +0200

    Added WithoutEnabledSettings scenario in NullabilityAnalysisTests

--- Src/ImplicitNullability.Plugin.Tests/NullabilityAnalysisTests.cs
+++ Src/ImplicitNullability.Plugin.Tests/NullabilityAnalysisTests.cs
@@ -1,11 +1,12 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using ImplicitNullability.Plugin.Settings;
 using ImplicitNullability.Plugin.Tests.Infrastructure;
 using JetBrains.Application.Settings;
+using JetBrains.ReSharper.Daemon.SolutionAnalysis;
 using JetBrains.Util;
 using NUnit.Framework;
-
 #if !RESHARPER8
 using JetBrains.ProjectModel;
 
@@ -16,18 +17,51 @@ namespace ImplicitNullability.Plugin.Tests
     public class NullabilityAnalysisTests : SampleSolutionTestBase
     {
         [Test]
-        public void TestInput()
+        public void WithEnabledInputAndRefParameters()
         {
-            Test("MIn", settingsStore => settingsStore.SetValue((ImplicitNullabilitySettings s) => s.EnableInputAndRefParameters, true));
+            Test(settingsStore => settingsStore.SetValue((ImplicitNullabilitySettings s) => s.EnableInputAndRefParameters, true),
+                issues =>
+                {
+                    // Fixation: minimum amount of warnings, selected files
+
+                    Assert.That(issues.Count, Is.GreaterThanOrEqualTo(70));
+
+                    var filePaths = issues.Select(x => x.GetSourceFile().Name).ToList();
+                    Assert.That(filePaths, Has.Some.EqualTo("AspxSample.aspx"));
+                    Assert.That(filePaths, Has.Some.EqualTo("RazorSample.cshtml"));
+                    Assert.That(filePaths, Has.Some.EqualTo("MethodsInputSample.cs"));
+                    Assert.That(filePaths, Has.Some.EqualTo("MethodsInputSampleTests.cs"));
+                },
+                "MIn");
         }
 
         [Test]
-        public void TestOutput()
+        public void WithEnabledOutParametersAndResult()
         {
-            Test("MOut", settingsStore => settingsStore.SetValue((ImplicitNullabilitySettings s) => s.EnableOutParametersAndResult, true));
+            Test(settingsStore => settingsStore.SetValue((ImplicitNullabilitySettings s) => s.EnableOutParametersAndResult, true),
+                issues =>
+                {
+                    // Fixation: minimum amount of warnings, selected files
+
+                    Assert.That(issues.Count, Is.GreaterThanOrEqualTo(25));
+
+                    var filePaths = issues.Select(x => x.GetSourceFile().Name).ToList();
+                    Assert.That(filePaths, Has.Some.EqualTo("AspxSample.aspx"));
+                    Assert.That(filePaths, Has.Some.EqualTo("RazorSample.cshtml"));
+                    Assert.That(filePaths, Has.Some.EqualTo("MethodsOutputSample.cs"));
+                    Assert.That(filePaths, Has.Some.EqualTo("MethodsOutputSampleTests.cs"));
+                },
+                "MOut");
         }
 
-        private void Test(string definedExpectedWarningSymbol, Action<IContextBoundSettingsStore> additionalChanges)
+        [Test]
+        public void WithoutEnabledSettings()
+        {
+            // Ensures that when the implicit nullability settings are disabled, the conditional expected warnings are *not* present.
+            Test(settingsStore => { }, issues => { });
+        }
+
+        private void Test(Action<IContextBoundSettingsStore> enableSettings, Action<IList<IIssue>> assert, string definedExpectedWarningSymbol = null)
         {
             UseSampleSolution(solution =>
             {
@@ -35,7 +69,7 @@ namespace ImplicitNullability.Plugin.Tests
                 {
                     settingsStore.SetValue((ImplicitNullabilitySettings s) => s.EnableInputAndRefParameters, false);
                     settingsStore.SetValue((ImplicitNullabilitySettings s) => s.EnableOutParametersAndResult, false);
-                    additionalChanges(settingsStore);
+                    enableSettings(settingsStore);
                 });
 
                 var projectFilesToAnalyze = solution.GetAllProjectFilesWithPathPrefix("NullabilityAnalysis\\");
@@ -43,15 +77,7 @@ namespace ImplicitNullability.Plugin.Tests
                 var issues = TestExpectedInspectionComments(solution, projectFilesToAnalyze, GetNullabilityAnalysisHighlightingTypes(),
                     definedExpectedWarningSymbol);
 
-                // Fixation: minimum amount of warnings, selected files
-
-                Assert.That(issues.Count, Is.GreaterThanOrEqualTo(25));
-
-                var filePaths = issues.Select(x => x.GetSourceFile().Name).ToList();
-                Assert.That(filePaths, Has.Some.EqualTo("AspxSample.aspx"));
-                Assert.That(filePaths, Has.Some.EqualTo("RazorSample.cshtml"));
-                Assert.That(filePaths, Has.Some.Matches("^Methods.*Sample.cs$"));
-                Assert.That(filePaths, Has.Some.Matches("^Methods.*SampleTests.cs$"));
+                assert(issues);
             });
         }
     }
