400ec4a489e9e0c0b2274efd6a2bb3293a1959be
Ulrich Buchgraber <ulrich.b@gmx.at>
2015-07-27 03:13:43 +0200

    Extended UnsafeCodeSample

--- Src/ImplicitNullability.Sample/NullabilityAnalysis/UnsafeCodeSample.cs
+++ Src/ImplicitNullability.Sample/NullabilityAnalysis/UnsafeCodeSample.cs
@@ -5,14 +5,28 @@ namespace ImplicitNullability.Sample.NullabilityAnalysis
 {
     public unsafe class UnsafeCodeSample
     {
-        public static void SomeMethod(int* a)
+        public static void Method(int* a)
         {
             ReSharper.TestValueAnalysisUnsafe(a, a == null /*Expect:ConditionIsAlwaysTrueOrFalse[MIn]*/);
         }
 
-        public static void SomeMethodWithCanbeNull([CanBeNull] int* a)
+        public static void MethodWithCanBeNull([CanBeNull] int* a)
         {
             ReSharper.TestValueAnalysisUnsafe(a /*Expect:AssignNullToNotNullAttribute*/, a == null);
+        }
+
+        public static void MethodWithRefAndOutParameter(ref int* refParam, out int* outParam)
+        {
+            // REPORTED false negative https://youtrack.jetbrains.com/issue/RSRP-427414:
+            ReSharper.TestValueAnalysisUnsafe(refParam, refParam == null);
+
+            refParam = null; /*Expect:AssignNullToNotNullAttribute[MIn]*/
+            outParam = null; /*Expect:AssignNullToNotNullAttribute[MOut]*/
+        }
+
+        public static int* Function()
+        {
+            return null; /*Expect:AssignNullToNotNullAttribute[MOut]*/
         }
     }
 }
