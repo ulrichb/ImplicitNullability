128dd6f88c75e9411fc536fa108b723962145176
Ulrich Buchgraber <ulrich.b@gmx.at>
2015-10-09 02:48:28 +0200

    WIP: Spiked highlighting

---
+++ Src/ImplicitNullability.Plugin/Highlighting/LoooolHighlighting.cs
@@ -0,0 +1,45 @@
+﻿using ImplicitNullability.Plugin.Highlighting;
+using System;
+using JetBrains.Annotations;
+using JetBrains.ReSharper.Psi.CSharp.ControlFlow;
+using JetBrains.ReSharper.Psi.Tree;
+using ReSharperExtensionsShared.Highlighting;
+#if RESHARPER8
+using JetBrains.ReSharper.Daemon;
+
+#else
+using JetBrains.ReSharper.Feature.Services.Daemon;
+
+#endif
+
+[assembly: RegisterConfigurableSeverity(
+    LoooolHighlighting.SeverityId,
+    null,
+    HighlightingGroupIds.CodeSmell,
+    LoooolHighlighting.Message,
+    LoooolHighlighting.Description,
+    Severity.WARNING,
+    solutionAnalysisRequired: false)]
+
+namespace ImplicitNullability.Plugin.Highlighting
+{
+    [ConfigurableSeverityHighlighting(
+        SeverityId,
+        "CSHARP",
+        OverlapResolve = OverlapResolveKind.WARNING,
+        ToolTipFormatString = Message)]
+    public class LoooolHighlighting : SimpleTreeNodeHighlightingBase<ITreeNode>
+    {
+        public const string SeverityId = "LoooolHighlighting";
+
+        public const string Message = "Unknown nullability to implicitly NotNull!";
+
+        public const string Description =
+            "LoooolHighlighting";
+
+        public LoooolHighlighting([NotNull] ITreeNode treeNode)
+            : base(treeNode, Message)
+        {
+        }
+    }
+}
--- Src/ImplicitNullability.Plugin/ImplicitNullabilityProblemAnalyzer.cs
+++ Src/ImplicitNullability.Plugin/ImplicitNullabilityProblemAnalyzer.cs
@@ -6,8 +6,12 @@ using ImplicitNullability.Plugin.Infrastructure;
 using JetBrains.Annotations;
 using JetBrains.ReSharper.Daemon.CSharp.Stages;
 using JetBrains.ReSharper.Daemon.Stages.Dispatcher;
+using JetBrains.ReSharper.Daemon.Stages.Utils;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.CodeAnnotations;
+using JetBrains.ReSharper.Psi.ControlFlow;
+using JetBrains.ReSharper.Psi.CSharp.ControlFlow;
+using JetBrains.ReSharper.Psi.CSharp.Impl.ControlFlow;
 using JetBrains.ReSharper.Psi.CSharp.Tree;
 using JetBrains.ReSharper.Psi.Tree;
 using JetBrains.Util;
@@ -63,11 +67,11 @@ namespace ImplicitNullability.Plugin
 
             var methodDeclaration = element as IMethodDeclaration;
             if (methodDeclaration?.DeclaredElement != null)
-                highlightingList.AddRange(HandleFunction(methodDeclaration.NameIdentifier, methodDeclaration.DeclaredElement));
+                highlightingList.AddRange(HandleFunction(methodDeclaration.NameIdentifier, methodDeclaration.DeclaredElement, methodDeclaration, data));
 
             var operatorDeclaration = element as IOperatorDeclaration;
             if (operatorDeclaration?.DeclaredElement != null)
-                highlightingList.AddRange(HandleFunction(operatorDeclaration.OperatorKeyword, operatorDeclaration.DeclaredElement));
+                highlightingList.AddRange(HandleFunction(operatorDeclaration.OperatorKeyword, operatorDeclaration.DeclaredElement, operatorDeclaration, data));
 
             var delegateDeclaration = element as IDelegateDeclaration;
             if (delegateDeclaration?.DeclaredElement != null)
@@ -109,7 +113,7 @@ namespace ImplicitNullability.Plugin
             }
         }
 
-        private IEnumerable<IHighlighting> HandleFunction([NotNull] ITreeNode declaration, [NotNull] IFunction function)
+        private IEnumerable<IHighlighting> HandleFunction([NotNull] ITreeNode declaration, [NotNull] IFunction function, IFunctionDeclaration methodDeclaration, ElementProblemAnalyzerData data)
         {
             var nullableAttributeMarks = GetNullableAttributeMarks(function);
 
@@ -117,6 +121,64 @@ namespace ImplicitNullability.Plugin
                 && _implicitNullabilityProvider.AnalyzeFunction(function) == CodeAnnotationNullableValue.CAN_BE_NULL)
             {
                 yield return new NotNullOnImplicitCanBeNullHighlighting(declaration);
+            }
+
+            // TODO: handle async methods (ItemNotNull/ItemCanBeNull)
+            if (!nullableAttributeMarks.Any() &&
+                _implicitNullabilityProvider.AnalyzeFunction(function) == CodeAnnotationNullableValue.NOT_NULL)
+            {
+                var cSharpControlFlowGraph = ControlFlowDaemonUtil.GetOrBuild(methodDeclaration, data) as ICSharpControlFlowGraph;
+
+                if (cSharpControlFlowGraph != null)
+                {
+                    var analysisResult = CSharpControlFlowGraphInspector.Inspect(cSharpControlFlowGraph, ValueAnalysisMode.OPTIMISTIC, node => ControlFlowDaemonUtil.GetOrBuild(node, data) as ICSharpControlFlowGraph);
+
+                    //var type = analysisResult.GetType();
+                    //var getVariableStateAt = type.GetMethod("GetVariableStateAt",
+                    //    BindingFlags.Instance | BindingFlags.NonPublic, null, new[] { typeof(IControlFlowElement), typeof(VariableInfo), typeof(bool), typeof(bool) }, null);
+                    //var findInfoByExpression = type.GetMethod("FindInfoByExpression", BindingFlags.Instance | BindingFlags.NonPublic);
+
+                    //return this.GetVariableStateAt(
+
+
+                    //    Enumerable.FirstOrDefault<IControlFlowElement>(
+
+
+                    //        this.ControlFlowGraph.GetLeafElementsFor((ITreeNode)expression)), 
+
+
+
+                    //    this.FindInfoByExpression((ICSharpExpression)expression), 
+
+
+                    //    false, 
+
+                    //    includeWeakNotNull);
+
+                    foreach (var node in methodDeclaration.ThisAndDescendants<IReturnStatement>().ToEnumerable())
+                    {
+                        //var cSharpControlFlowNullReferenceState =
+                        //    (CSharpControlFlowNullReferenceState)
+                        //        getVariableStateAt.Invoke(analysisResult, new object[]
+                        //        {
+                        //            cSharpControlFlowGraph.GetLeafElementsFor(node.Value).FirstOrDefault(),
+                        //            findInfoByExpression.Invoke(analysisResult, new object[ ] {node.Value}),
+                        //            false,
+                        //            true
+                        //        });
+
+
+
+
+
+                        //CSharpControlFlowNullReferenceState cSharpControlFlowNullReferenceState = analysisResult.GetExpressionNullReferenceState((IReferenceExpression)node.Value, true);
+                        if (analysisResult.SuggestReturnValueAnnotationAttribute == CSharpControlFlowNullReferenceState.UNKNOWN)
+                        {
+                            yield return new LoooolHighlighting(node);
+                        }
+
+                    }
+                }
             }
         }
 
