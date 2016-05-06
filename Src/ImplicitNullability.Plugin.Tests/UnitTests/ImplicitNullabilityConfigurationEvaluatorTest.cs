d15b2d80a584db3e384afaf86f834956036bbc93
Ulrich Buchgraber <ulrich.b@gmx.at>
2016-05-06 20:43:30 +0200

    Removed unnecessary default value

--- Src/ImplicitNullability.Plugin.Tests/UnitTests/ImplicitNullabilityConfigurationEvaluatorTest.cs
+++ Src/ImplicitNullability.Plugin.Tests/UnitTests/ImplicitNullabilityConfigurationEvaluatorTest.cs
@@ -113,7 +113,7 @@ namespace ImplicitNullability.Plugin.Tests.UnitTests
 
         private ImplicitNullabilityConfiguration GetImplicitNullabilityConfigurationFor(
             [NotNull] string testInput,
-            Action<IContextBoundSettingsStore> changeSolutionSettings = null,
+            [NotNull] Action<IContextBoundSettingsStore> changeSolutionSettings,
             Action<IContextBoundSettingsStore> changeProjectSettings = null)
         {
             ImplicitNullabilityConfiguration? result = null;
@@ -129,7 +129,7 @@ namespace ImplicitNullability.Plugin.Tests.UnitTests
                         var sut = solution.GetComponent<ImplicitNullabilityConfigurationEvaluator>();
                         var settingsStore = project.GetComponent<SettingsStore>();
 
-                        changeSolutionSettings?.Invoke(
+                        changeSolutionSettings.Invoke(
                             settingsStore.BindToContextTransient(ContextRange.ManuallyRestrictWritesToOneContext(solution.ToDataContext())));
 
                         changeProjectSettings?.Invoke(
