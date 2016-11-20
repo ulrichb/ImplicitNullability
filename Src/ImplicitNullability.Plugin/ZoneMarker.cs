ce0854174ade23c0f60e830535409b9502f059cf
Ulrich Buchgraber <ulrich.b@gmx.at>
2016-11-20 20:25:30 +0100

    Fix ZoneMarker: replace DaemonEngineZone with DaemonZone and add missing ILanguageAspZone

--- Src/ImplicitNullability.Plugin/ZoneMarker.cs
+++ Src/ImplicitNullability.Plugin/ZoneMarker.cs
@@ -1,13 +1,17 @@
 ﻿using JetBrains.Application.BuildScript.Application.Zones;
 using JetBrains.ReSharper.Feature.Services.Daemon;
 using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.Asp;
 using JetBrains.ReSharper.Psi.CSharp;
 
 namespace ImplicitNullability.Plugin
 {
     [ZoneDefinition]
     [ZoneDefinitionConfigurableFeature(AssemblyConsts.Title, AssemblyConsts.Description, /*IsInProductSection:*/ false)]
-    public interface IImplicitNullabilityZone : IPsiLanguageZone, IRequire<ILanguageCSharpZone>, IRequire<DaemonEngineZone>
+    public interface IImplicitNullabilityZone : IPsiLanguageZone,
+        IRequire<ILanguageCSharpZone>,
+        IRequire<DaemonZone>,
+        IRequire<ILanguageAspZone /* for AspImplicitTypeMember */>
     {
     }
 
