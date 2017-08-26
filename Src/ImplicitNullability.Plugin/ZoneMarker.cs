8f42b67a6408ab463b451bd00eaeb87abc1c370f
Ulrich Buchgraber <ulrich.b@gmx.at>
2017-08-26 15:51:07 +0200

    Switch to default assembly title

--- Src/ImplicitNullability.Plugin/ZoneMarker.cs
+++ Src/ImplicitNullability.Plugin/ZoneMarker.cs
@@ -5,8 +5,23 @@ using JetBrains.ReSharper.Psi.CSharp;
 
 namespace ImplicitNullability.Plugin
 {
+    internal static class PluginConsts
+    {
+        public const string Title =
+                "Implicit Nullability"
+#if DEBUG
+                + " (Debug Build)"
+#endif
+            ;
+
+        // Note: Text is duplicated in the nuspec.
+        public const string Description =
+            "Extends ReSharper's static analysis by changing specific, configurable elements to be [NotNull] by default";
+    }
+
+
     [ZoneDefinition]
-    [ZoneDefinitionConfigurableFeature(AssemblyConsts.Title, AssemblyConsts.Description, IsInProductSection: false)]
+    [ZoneDefinitionConfigurableFeature(PluginConsts.Title, PluginConsts.Description, IsInProductSection: false)]
     public interface IImplicitNullabilityZone : IPsiLanguageZone,
         IRequire<ILanguageCSharpZone>,
         IRequire<DaemonZone>
