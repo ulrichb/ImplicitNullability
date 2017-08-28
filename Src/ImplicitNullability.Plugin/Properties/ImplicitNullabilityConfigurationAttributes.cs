bf2997ee02b98427b221387ea47850ecf83da53c
Ulrich Buchgraber <ulrich.b@gmx.at>
2017-08-28 05:04:32 +0200

    Enable ImplicitNullability for properties in own solution

--- Src/ImplicitNullability.Plugin/Properties/ImplicitNullabilityConfigurationAttributes.cs
+++ Src/ImplicitNullability.Plugin/Properties/ImplicitNullabilityConfigurationAttributes.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyMetadata("ImplicitNullability.AppliesTo", "InputParameters, RefParameters, OutParametersAndResult, Fields")]
+[assembly: AssemblyMetadata("ImplicitNullability.AppliesTo", "InputParameters, RefParameters, OutParametersAndResult, Fields, Properties")]
 [assembly: AssemblyMetadata("ImplicitNullability.Fields", "RestrictToReadonly")]
+[assembly: AssemblyMetadata("ImplicitNullability.Properties", "RestrictToGetterOnly")]
